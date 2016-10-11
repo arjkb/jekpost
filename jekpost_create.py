bc7290502fa7a9182c5b766802a9162b95ad0b3f
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2016-10-11 21:17:41 +0530

    moving current functionalities to main()

--- jekpost_create.py
+++ jekpost_create.py
@@ -1,7 +1,11 @@
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('title', help='Post title')
-args = parser.parse_args()
-post_title = args.title
-print(" Post Title: ", post_title)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('title', help='Post title')
+    args = parser.parse_args()
+    post_title = args.title
+    print(" Post Title: ", post_title)
+
+if __name__ == '__main__':
+    main()

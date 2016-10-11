c30495ed85976169a26e9710868c74e40d50b663
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2016-10-11 21:34:54 +0530

    Generating proper filename. Stripping the original title from the command-line

--- jekpost_create.py
+++ jekpost_create.py
@@ -1,6 +1,11 @@
 import argparse
 from datetime import date
 
+def get_filename(post_title, date_prefix):
+    title_formatted = post_title.replace(' ', '-')
+    filename = date_prefix + '-' + title_formatted + '.md'
+    return filename
+
 def get_current_date():
     today = date.today()
     year = str(today.year)
@@ -13,10 +18,12 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('title', help='Post title')
     args = parser.parse_args()
-    post_title = args.title
+    post_title = args.title.strip()
     date_prefix = get_current_date()
+    filename = get_filename(post_title=post_title, date_prefix=date_prefix)
     print(" Post Title: ", post_title)
     print(" Date Prefix: ", date_prefix)
+    print(" Filename: ", filename)
 
 if __name__ == '__main__':
     main()

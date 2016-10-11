a689630738de84b52dd48f7e83452d6394601b78
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2016-10-12 01:02:13 +0530

    function to extract the posts folder path from the yaml file

--- jekpost_create.py
+++ jekpost_create.py
@@ -1,4 +1,5 @@
 import argparse
+import yaml
 from datetime import date
 from string import Template
 
@@ -19,6 +20,16 @@ def generate_post_file(filename, title):
 
     with open(filename, 'w', encoding='utf-8') as actual_file:
         actual_file.write(actual_file_content)
+
+def read_post_path(config_filename):
+    """
+    Read the config yaml file, and return the filepath to the Jekyll
+    folder that contains the posts
+    """
+
+    with open(config_filename, 'r', encoding='utf-8') as config_file:
+        doc = yaml.load(config_file)
+    return doc['posts_location']
 
 def get_filename(post_title, date_prefix):
     title_formatted = post_title.replace(' ', '-')
@@ -44,6 +55,9 @@ def main():
     print(" Date Prefix: ", date_prefix)
     print(" Filename: ", filename)
     generate_post_file(filename=filename, title=post_title)
+
+    p = read_post_path('jekpost_config.yaml')
+    print(p)
 
 if __name__ == '__main__':
     main()

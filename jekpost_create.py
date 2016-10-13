3a112f3329be75358980c41c53deccff7cfe0e95
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2016-10-13 10:50:10 +0530

    handling the exception when the post already exists at the destination

--- jekpost_create.py
+++ jekpost_create.py
@@ -1,6 +1,9 @@
 import argparse
 import yaml
 import shutil
+import os
+
+
 from datetime import date
 from string import Template
 
@@ -13,6 +16,8 @@ def generate_post_file(filename, title):
     title -- title of the post
     """
 
+    print(" Generating post file...", end="")
+
     with open('post.template', 'r', encoding='utf-8') as template_file:
         template_file_content = template_file.read()
     t = Template(template_file_content)
@@ -21,6 +26,7 @@ def generate_post_file(filename, title):
 
     with open(filename, 'w', encoding='utf-8') as actual_file:
         actual_file.write(actual_file_content)
+    print(" done!")
 
 def read_post_path(config_filename):
     """
@@ -28,8 +34,10 @@ def read_post_path(config_filename):
     folder that contains the posts
     """
 
+    print(" Reading config...", end="")
     with open(config_filename, 'r', encoding='utf-8') as config_file:
         doc = yaml.load(config_file)
+    print(" done!")
     return doc['posts_location']
 
 def get_filename(post_title, date_prefix):
@@ -49,16 +57,24 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('title', help='Post title')
     args = parser.parse_args()
+
     post_title = args.title.strip()
     date_prefix = get_current_date()
     filename = get_filename(post_title=post_title, date_prefix=date_prefix)
     print(" Post Title: ", post_title)
-    print(" Date Prefix: ", date_prefix)
     print(" Filename: ", filename)
+
     generate_post_file(filename=filename, title=post_title)
     posts_path = read_post_path('jekpost_config.yaml')
-    print(posts_path)
-    shutil.move(src=filename, dst=posts_path)
+
+    try:
+        print(" Moving to: ", posts_path)
+        shutil.move(src=filename, dst=posts_path)
+    except Exception as e:
+        print("\n ERROR: ", e)
+        os.remove(filename)  # remove local copy
+    else:
+        print("\n New post created!\n Happy blogging!")
 
 if __name__ == '__main__':
     main()

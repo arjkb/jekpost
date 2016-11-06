d832547e4bb2e0fbf4c509ddf288f502a3b43a63
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2016-11-06 23:31:23 +0530

    adding testcase to check that make_filename() works as expected

--- tests/jekpost_tests.py
+++ tests/jekpost_tests.py
@@ -11,15 +11,23 @@ class JekpostTests(unittest.TestCase):
             1-NOV-2015  (2015-11-01)
             11-JAN-2015 (2015-01-11)
         """
-        sample_dates = [    (date(2016, 12, 31), '2016-12-31'),
+        sample_dates = [    (date(2014, 12, 31), '2014-12-31'),
                             (date(2015, 11, 1), '2015-11-01'),
                             (date(2015, 1, 11), '2015-01-11')
                        ]
-                       
+
         for date_object, expected_date in sample_dates:
             with self.subTest(i=date_object):
                 formatted_date = jek.get_date_formatted(date_object)
                 self.assertEqual(formatted_date, expected_date)
+
+    def test_make_filename(self):
+        date_formatted = '2014-12-31'
+        title = 'Post 01'
+        expected_filename = '2014-12-31-Post-01.md'
+
+        result_filename = jek.make_filename(title, date_formatted)
+        self.assertEqual(result_filename, expected_filename)
 
 if __name__ == '__main__':
     unittest.main()

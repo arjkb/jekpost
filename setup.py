9d0e3b6ee0b02c0eba6d0c5a7d1110899b2de217
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2016-11-12 00:17:12 +0530

    created basic setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup = (
+    name='JekPost',
+    version='0.1.0',
+    author='Arjun Krishna Babu',
+    author_email='arjunkrishnababu96@gmail.com',
+    packages=['jekpost'],
+    url='',
+    license='LICENSE.txt',
+    description='Package to ease the process of creating a new Jekyll post'
+    long_description=open('README.rst').read(),
+    install_requires=[
+        "python >= 3.5"
+    ],
+)

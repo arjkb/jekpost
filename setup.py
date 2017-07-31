6c94f1ade12d9e36ab25188f515a146fa65ca098
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-07-31 12:16:19 +0530

    add OS classifier: POSIX

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
         "Topic :: Utilities",
     ],
 )

5c13d44851e554cf201b52a025a7c4f81c30f548
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-08-12 19:34:19 +0530

    update setup.py

--- setup.py
+++ setup.py
@@ -2,24 +2,29 @@ from setuptools import setup
 
 setup(
     name='JekPost',
-    version='1.1.0',
+    version='2.0.0',
     author='Arjun Krishna Babu',
     author_email='arjunkrishnababu96@gmail.com',
     packages=['jekpost'],
     url='https://github.com/arjunkrishnababu96/jekpost',
-    license='LICENSE.txt',
+    license='MIT',
     description='Package to ease the process of creating a new Jekyll post',
     long_description=open('README.rst').read(),
     classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
+        "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: MIT License",
         "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Topic :: Utilities",
     ],
     entry_points={
         'console_scripts':  [
-            'jekpost_create = jekpost.jekpost_create:main'
+            'jekpost_create = jekpost.jekpost:main',
         ],
     }
 )
